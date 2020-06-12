661036a1de0ae131b3345e6ef9d7d3b47812c5ed
Randy Lai <randy.cs.lai@gmail.com>
2020-06-12 14:36:14 -0700

    remove unused code

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,6 @@
 int_to_bytes <- function(n) {
+    # compare it to writeBin(n, raw(0), endian = "big")
+    # it supports input up to the size around 2^.Machine$double.digits
     b <- raw(8)
     for (i in seq_along(b)) {
         b[8 - i + 1] <- as.raw(n %% 256L)
@@ -14,31 +16,3 @@ dyn_extract <- function(h) {
     u[1] <- u[1] & as.raw(0x7f)
     sum(as.integer(u) * 256 ^ (3:0))
 }
-
-
-# re_match <- function(pattern, x) {
-#     regmatches(x, regexec(pattern, x, perl = T))
-# }
-
-
-# prase_uri <- function(uri) {
-#     if (grepl("^otpauth://", uri)) {
-#         base32 <- TRUE
-#         m <- re_match("uri=([^&$]+)", uri)[[1]]
-#         uri <- m[2]
-#         if (grepl("^otpauth://hotp", uri)) {
-#             if (is.null(type)) {
-#                 type <- "hotp"
-#             } else if (type != "hotp") {
-#                 stop("uri type is hotp")
-#             }
-#         } else if (grepl("^otpauth://totp", uri)) {
-#             type <- "totp"
-#             if (is.null(type)) {
-#                 type <- "totp"
-#             } else if (type != "totp") {
-#                 stop("uri type is totp")
-#             }
-#         }
-#     }
-# }
